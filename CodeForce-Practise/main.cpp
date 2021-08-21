5ae2340f9a91946035dac96769a68806c58b74b9
Dev Patel <devpatel8907@gmail.com>
2021-08-21 10:26:18 +0530

    Initial Commit

---
+++ CodeForce-Practise/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CodeForce-Practise
+//
+//  Created by Dev Patel on 21/08/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
