36ecd6627a96b25479d72eaf6df88b13fa286343
Dev Patel <devpatel8907@gmail.com>
2021-08-21 10:33:59 +0530

    test commit

---
+++ Codeforce Not Adjacent Matrix/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Codeforce Not Adjacent Matrix
+//
+//  Created by Dev Patel on 21/08/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    int t;
+    cin>>t;
+    return 0;
+}
